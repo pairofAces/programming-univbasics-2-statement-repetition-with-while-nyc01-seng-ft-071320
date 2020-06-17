d38c65506deadf545c0f4a9417f91ced76d6d9cb
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-17 19:08:26 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,11 +1,5 @@
 # Write your code here
-magic_exit_number = 0
 count = 10
 
-while count < 11 do
-  break if count == magic_exit_number
-  puts "#{count}"
-  count -= 1
-else
-  puts "Happy New Year!"
-end
+while count >= 1 do
+ 
